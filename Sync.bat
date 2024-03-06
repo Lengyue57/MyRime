570ffe986cd97e8ad8a6cebce6327e80c4b8f2f4
Lengyue57 <1210625283@qq.com>
2024-03-06 11:50:54 +0800

    Update

--- Sync.bat
+++ Sync.bat
@@ -1,5 +1,5 @@
 git pull origin master:master
 "C:/Program Files (x86)/Rime/weasel-0.15.0/WeaselDeployer.exe" /sync
-git add *
+git add */*.userdb.txt
 git commit -m "Auto commit"
 git push origin master:master
